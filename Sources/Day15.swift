4dd1a1cf95c63a64007cc434d34b6906fa6a3879
Illia Akhaiev <26223450+grandboum@users.noreply.github.com>
2024-12-16 23:23:07 +0100

    Added stub for solution for Day15

---
+++ Sources/Day15.swift
@@ -0,0 +1,20 @@
+import Algorithms
+import DequeModule
+import RegexBuilder
+import Foundation
+
+struct Day15: AdventDay, @unchecked Sendable {
+  var data: String
+
+  func part1() -> Any {
+    return true
+  }
+  
+  func part2() -> Any {
+    return true
+  }
+
+  func parseInput() -> [Int] {
+    return []
+  }
+}
---
+++ Tests/Day15.swift
@@ -0,0 +1,69 @@
+import Testing
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+struct Day15Tests {
+  // Smoke test data provided in the challenge question
+  let large_file = """
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+
+    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """
+  let smaller_file = """
+    ########
+    #..O.O.#
+    ##@.O..#
+    #...O..#
+    #.#.O..#
+    #...O..#
+    #......#
+    ########
+
+    <^^>>>vv<v>>v<<
+    """
+
+  let largerAnswer = 10092
+  let largerAnswerPt2 = 9021
+  let smallerAnswer = 2028
+
+  let firsPart = 1430536
+  let secondPart = 1452348
+
+  let input: [Int] = [
+  ]
+
+  @Test func testParsing() async throws {
+    let challenge = Day15(data: large_file)
+    #expect(challenge.parseInput() == input)
+  }
+
+  @Test func testPart1() async throws {
+    let challenge = Day15(data: large_file)
+    #expect(String(describing: challenge.part1()) == "12")
+  }
+
+  @Test func testPart2() async throws {
+    let challenge = Day15(data: large_file)
+    #expect(String(describing: challenge.part1()) == "480")
+  }
+}
